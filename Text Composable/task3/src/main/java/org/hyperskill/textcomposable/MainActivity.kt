6c8d6aed29372590a2eb1516fc1a7ab692d80df9
RazoTRON <vlad05966@gmail.com>
2023-01-12 14:53:14 +0300

    initial commit

---
+++ Text Composable/task3/src/main/java/org/hyperskill/textcomposable/MainActivity.kt
@@ -0,0 +1,27 @@
+package org.hyperskill.textcomposable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.withStyle
+
+// DO NOT import androidx.compose.material.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InconsolataText("Font text")
+        }
+    }
+}
+
+@Composable
+fun InconsolataText(text: String) {
+
+}
